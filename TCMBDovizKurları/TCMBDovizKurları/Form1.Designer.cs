b308bb43b1fc11b5ad3ea2a0174d47995a45ab0f
BURAK AYDIN <burakaydin61@hotmail.com>
2018-01-20 14:09:33 +0300

    TCMBDovizKurları

---
+++ TCMBDovizKurları/TCMBDovizKurları/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace TCMBDovizKurları
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Kod = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Isim = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Alis = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Satis = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Kod,
+            this.Isim,
+            this.Name,
+            this.Alis,
+            this.Satis});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(726, 239);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Kod
+            // 
+            this.Kod.HeaderText = "Kod";
+            this.Kod.Name = "Kod";
+            this.Kod.ReadOnly = true;
+            // 
+            // Isim
+            // 
+            this.Isim.HeaderText = "İsim";
+            this.Isim.Name = "Isim";
+            this.Isim.ReadOnly = true;
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "Name";
+            this.Name.Name = "Name";
+            this.Name.ReadOnly = true;
+            // 
+            // Alis
+            // 
+            this.Alis.HeaderText = "Alış";
+            this.Alis.Name = "Alis";
+            this.Alis.ReadOnly = true;
+            // 
+            // Satis
+            // 
+            this.Satis.HeaderText = "Satış";
+            this.Satis.Name = "Satis";
+            this.Satis.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Verilerin Çekildiği Tarih:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(236, 261);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "tarih";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(585, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Yenile";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Red;
+            this.linkLabel1.Location = new System.Drawing.Point(653, 302);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(61, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "@61baydin";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 324);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            //this.Name = "Form1";
+            this.Text = "TCMB Döviz Kurları";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Kod;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Isim;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Alis;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Satis;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ TCMBDovizKurları/TCMBDovizKurları/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace TCMBDovizKurları
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XmlDocument dosya = new XmlDocument();
+            dosya.Load("http://www.tcmb.gov.tr/kurlar/today.xml");
+            XmlNodeList items = dosya.SelectNodes("Tarih_Date/Currency");
+            foreach (XmlNode item in items)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells["Kod"].Value =
+                     item.Attributes["Kod"].InnerText;
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells["Isim"].Value =
+                    item.SelectSingleNode("Isim").InnerText;
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells["Name"].Value =
+                    item.SelectSingleNode("CurrencyName").InnerText;
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells["Alis"].Value =
+                    item.SelectSingleNode("ForexBuying").InnerText;
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells["Satis"].Value =
+                    item.SelectSingleNode("ForexSelling").InnerText;
+            }
+            DateTime tarih = Convert.ToDateTime(dosya.SelectSingleNode("//Tarih_Date").Attributes["Tarih"].Value);
+            label2.Text = tarih.ToShortDateString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/61baydin");
+        }
+    }
+}
